6fe3cc1b98016db414913759f53baccafe274a75
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-02-02 01:52:52 +0530

    CartSLice updated

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,7 +7,15 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const newItem = action.payload;
+      const existingItem = state.items.find(item=> item.id === newItem.id);
+      if (existingItem) {
+        // If item already exists, update its quantity
+        existingItem.quantity += newItem.quantity;
+      } else {
+        // If it's a new item, add it to the cart with the quantity
+        state.items.push({ ...newItem, quantity: newItem.quantity || 1 });
+      }
     },
     removeItem: (state, action) => {
     },
